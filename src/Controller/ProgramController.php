a51a228dd9559ed53ff79fc9c9d0ed2237924227
Quentin <quentin.burty@gmail.com>
2021-05-31 11:15:28 +0200

    Symfo 05

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -17,4 +17,14 @@ class ProgramController extends AbstractController
        'website' => 'Wild Séries',
     ]);
     }
+
+    /**
+     * @Route("/programs/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="program_id")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+       'id' => $id,
+    ]);
+    }
 }
